59648c3431d0da6afbdcffa79c9c9a6cce6887d7
Alex <aevangelou10@gmail.com>
2019-02-17 00:08:55 -0500

    may have to revert :(

---
+++ src/components/contact/BusinessCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../../stylesheets/Contact.css';
+
+class BusinessCard extends Component {
+    render() {
+        return (
+            <div className="businessCard">
+            </div>
+        );
+    }
+}
+
+export default BusinessCard;
--- src/components/contact/ContactMain.js
+++ src/components/contact/ContactMain.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
-import SimpleBar from 'simplebar-react';
-import 'simplebar/dist/simplebar.min.css';
-import '../../stylesheets/Contact.css';
+import BusinessCard from './BusinessCard';
 import '../../App.css';
 
 class ContactMain extends Component {
     render() {
         return (
             <div className="AppBody">
-                <SimpleBar>
-                    <h1>Contact</h1>
-                </SimpleBar>
+                <BusinessCard/>
             </div>
         );
     }
